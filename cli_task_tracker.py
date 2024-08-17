1f1c99ec6de9f78f8af6fc15e388e47e5006f490
SCharles <97812129+SCharlesM@users.noreply.github.com>
2024-08-17 13:46:54 +0100

    added input loop structure for adding different inputs

--- cli_task_tracker.py
+++ cli_task_tracker.py
@@ -1,5 +1,23 @@
 #cli_tasker_tracker.py
 #from the roadmap.sh project suggestions page
 #
-print('welcome to cli task tracker')
-command = input('what would you like to do:')
+
+print('\nWelcome to cli task tracker....')
+loop = True
+
+while (loop) :
+    command = input('\ntask-cli:')
+
+    if command == 'add' :
+        print('add to JSON file')
+
+    elif command == 'delete' :
+        print('deleting from JSON file')
+    
+    elif command == 'end' :
+        break
+
+    else :
+        print('incorrect input')
+
+print('end of cli-task')
