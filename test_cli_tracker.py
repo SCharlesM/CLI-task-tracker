5c8147dbb6a4947c43766c703a9fdd541ac50da7
SCharles <97812129+SCharlesM@users.noreply.github.com>
2024-08-26 13:24:41 +0100

    adding changes to list_data and test file

--- test_cli_tracker.py
+++ test_cli_tracker.py
@@ -1,11 +1,16 @@
 #test file whilst working on cli_task_tracker.py
-from cli_task_tracker import add_to_list, delete_from_list, update_list
+from cli_task_tracker import add_to_list, delete_from_list, update_list, print_task_list
 
+#test add_to_list by adding 4 tasks to the list
 add_to_list("make a list")
 add_to_list("go to shops")
 add_to_list("buy groceries")
 add_to_list("cook dinner")
 
-delete_from_list(1)
+#test print function
+print_task_list()
 
-update_list(2, "go to shops and post office")
+#delete_from_list(1)
+
+#update_list(2, "go to shops and post office")
+
